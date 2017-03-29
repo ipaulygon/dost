8ede98dafcceb728d1f2bbd3bbb39cc104c4cd5f
ipaulygon <paulandrei@ymail.com>
2017-03-30 01:27:20 +0800

    sample bmi

--- src/pages/bmi/bmi.ts
+++ src/pages/bmi/bmi.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SQLite } from 'ionic-native';
 
 /*
   Generated class for the Bmi page.
@@ -12,11 +14,31 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'bmi.html'
 })
 export class BmiPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  bmiForm : FormGroup;
+  submitAttempt : boolean = false;
+  kilo : boolean = true;
+  pound : boolean = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder,) {
+    this.bmiForm = formBuilder.group({
+        gender: ['', Validators.compose([Validators.required])],
+        birth: ['', Validators.compose([Validators.required])],
+        weight: ['kg', Validators.compose([Validators.required])],
+        noWeight: ['0', Validators.compose([Validators.required])],
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BmiPage');
+  }
+
+  weightChange(){
+    if(this.bmiForm.value.weight=="kg"){
+      this.kilo = true;
+      this.pound = false;
+    }else{
+      this.kilo = false;
+      this.pound = true;
+    }
   }
 
 }
