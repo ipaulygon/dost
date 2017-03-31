3aa4e9fdad670fb74925abafb396b525e67dc24d
MisaMedusa <elocin_oxcim@yahoo.com>
2017-04-01 01:32:51 +0800

    if adult, show height input -- done

--- src/pages/energy-requirement/energy-requirement.ts
+++ src/pages/energy-requirement/energy-requirement.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /*
   Generated class for the EnergyRequirement page.
@@ -14,19 +15,41 @@ import { NavController, NavParams } from 'ionic-angular';
 export class EnergyRequirementPage {
 
   gender:any = "Male";
-  ageRange:any;
+  ageRange:any = "--Age Group--";
 
-  height:any;
-  heightMin: any;
-  heightMax: any;
-  unitMeasure: any = "--Select Unit--";
-  unitMeasureAbbrev: any;
+  height:any = 122;
+  heightMin: any = 122;
+  heightMax: any = 243;
+  unitMeasure: any = "Centimeters";
+  unitMeasureAbbrev:any = 'cm';
+
+  energyRqmt: any;
+
+  formGroup: FormGroup;
+
+  isAdult = false;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder:FormBuilder) {
+    this.formGroup = formBuilder.group({
+      gender: ['', Validators.compose([Validators.required])],
+      ageRange: ['', Validators.compose([Validators.required])],
+      unitMeasure: ['',Validators.compose([Validators.required])],
+      height: ['',Validators.compose([Validators.pattern('/^[0-9]+(\.[0-9]{1,2})?$/'),Validators.required])]
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EnergyRequirementPage');
+  }
+
+  ageRangeChanged(){
+    if(this.ageRange == "19+"){
+      this.isAdult = true;
+    }
+    else{
+      this.isAdult = false;
+    }
   }
 
   unitChanged(){
@@ -37,14 +60,31 @@ export class EnergyRequirementPage {
     }
     else if(this.unitMeasure == 'Feet'){
       this.heightMin = 4;
-      this.heightMax = 7.97;
+      this.heightMax = 8;
       this.unitMeasureAbbrev = 'ft';
     } 
     else if(this.unitMeasure == 'Inches'){
-      this.heightMin = 48.03;
-      this.heightMax = 95.67;
+      this.heightMin = 48;
+      this.heightMax = 95;
       this.unitMeasureAbbrev = 'in';
     }
+  }//end of unitChanged
+
+  isFormComplete(){
+     if(this.formGroup.valid){
+      if(this.gender == 'Male'){
+        if(this.ageRange == '1-2'){
+          this.energyRqmt = '1000 kcal';
+        }
+        else if(this.ageRange == '3-5'){
+          this.energyRqmt = '1350 kcal';
+        }
+      }
+      else{
+
+      }
+     }//if formgroup is valid
   }
+
 
 }
