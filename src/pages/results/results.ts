c9a5ac25fcfe82945290ccee02fa029239415fe7
ipaulygon <paulandrei@ymail.com>
2017-04-22 16:14:25 +0800

    user profile energy gap

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 /*
   Generated class for the Results page.
@@ -14,6 +15,7 @@ import { NavController, NavParams, ToastController } from 'ionic-angular';
 })
 export class ResultsPage {
   
+  macroForm: FormGroup;
   gender: string;
   birthday: string;
   weight: number;
@@ -21,7 +23,7 @@ export class ResultsPage {
   waist: number;
   hip: number;
   age: number;
-
+  
   //bmi
   bmi: any;
   bmiStatus : any;
@@ -41,57 +43,61 @@ export class ResultsPage {
   dbwSee : boolean = false;
 
   //energy
-  activity: any = 30;
-  energy: any = 0;
-  energyCard : boolean = false;
-  energyMessage: any;
+  macroRange: boolean = false;
+  minProtein: number = 6;
+  maxProtein: number = 15;
+  minFat: number = 25;
+  maxFat: number = 35;
+  minCarb: number = 50;
+  maxCarb: number = 69;
+  protein: number = 15;
+  fat: number = 30;
+  carb: number = 55;
+  proteinReset: number;
+  fatReset: number;
+  carbReset: number;
+  proteinG: number;
+  fatG: number;
+  carbG: number;
+  macroPercent: number = 100;
+  energyCard: boolean = false;
   energySee : boolean = false;
-  modifyDistribution : boolean = false;
-  minCarbs : number = 0.0;
-  maxCarbs : number = 0.0;
-  minProtein : number = 0.0;
-  maxProtein : number = 0.0;
-  minFat : number = 0.0;
-  maxFat : number = 0.0;
+  kcal: number;
+  activity: number = 30;
 
-  carbsPercentage : number = 65;
-  proteinPercentage : number = 15;
-  fatPercentage : number = 20;
-
-  carbsGrams : number = 0.0;
-  proteinGrams : number = 0.0;
-  fatGrams : number = 0.0;
-
-  totalPercentage : number = 0.0;
-
-  //
+  //waistCir
   waistCircOnRisk: boolean = false;
   waistCirCard: boolean = true;
   waistCirMessage: any;
   waistCirSee : boolean = false;
 
-  //
+  //waist-Hip
   whRatio: number = 0.0;
   whOnRisk: boolean = false;
   whCard: boolean = true;
   whMessage: any;
   whSee : boolean = false;
 
-  //
+  //waist-height
   whgtRatio: number = 0.0;
   whgtOnRisk: boolean = false;
   whgtCard: boolean = false;
   whgtMessage: any = "Height";
   whgtSee : boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public formBuilder:FormBuilder) {
     this.gender = this.navParams.get("gender");
     this.birthday = this.navParams.get("birthday");
     this.weight = this.navParams.get("weight");
     this.height = this.navParams.get("height");
     this.waist = this.navParams.get("waist");
     this.hip = this.navParams.get("hip");
-
+    this.age = this.getAge();
+    this.macroForm = formBuilder.group({
+      proteinRange: [this.protein],
+      fatRange: [this.fat],
+      carbRange: [this.carb],
+    });
     if(this.height!=0){
       this.whgtCard = true;
       if(this.birthday!=''){
@@ -109,19 +115,15 @@ export class ResultsPage {
     }else{
       this.whgtCard = false;
     }
-
-    this.carbsPercentage = 0.65;
-    this.proteinPercentage = 0.15;
-    this.fatPercentage = 0.20;
-
     this.bmiResult();
     this.dbwResult();
     this.energyResult();
     this.waistCircumResult();
     this.waistHipResult();
     this.waistHeightResult();
-
-    //this.calculateDistribution();
+    this.macroForm.valueChanges
+		.debounceTime(100)
+		.subscribe(data => this.computePercent());
   }
 
   ionViewDidLoad() {
@@ -162,160 +164,167 @@ export class ResultsPage {
     }
   }
 
-  modifyDistributionClicked(){
-    this.modifyDistribution = true;
-    this.energySee = true;
+  macro(){
+    if(!this.macroRange){
+      this.proteinReset = this.protein;
+      this.fatReset = this.fat;
+      this.carbReset = this.carb;
+    }
+    this.macroRange = (this.macroRange) ? false : true;
+  }
 
-    let age = this.getAge();
+  reset(){
+    this.macroForm.controls['proteinRange'].setValue(this.proteinReset);
+    this.macroForm.controls['fatRange'].setValue(this.fatReset);
+    this.macroForm.controls['carbRange'].setValue(this.carbReset);
+    this.protein = this.proteinReset;
+    this.fat = this.fatReset;
+    this.carb = this.carbReset;
+    this.computePercent();
+  }
 
-    if(age == 1 || age == 2){
-      this.minProtein = 6;
-      this.maxProtein = 15;
+  computePercent(){
+    this.macroPercent = this.protein + this.fat + this.carb;
+  }
 
-      this.minFat = 20;
-      this.maxFat = 35;
- 
-      this.minCarbs = 50;
-      this.maxCarbs= 69;
-    }else if(age >= 19){
-      this.minProtein = 10;
-      this.maxProtein = 15;
+  proteinChange(){
+    this.protein = this.macroForm.value.proteinRange;
+    let proteinG = (this.kcal*(this.protein/100))/4;
+    this.proteinG = Math.round(proteinG/5)*5;
+  }
+  fatChange(){
+    this.fat = this.macroForm.value.fatRange;
+    let fatG = (this.kcal*(this.fat/100))/9;
+    this.fatG = Math.round(fatG/5)*5;
+  }
 
-      this.minFat = 15;
-      this.maxFat = 30;
- 
-      this.minCarbs = 55;
-      this.maxCarbs = 75;
+  carbChange(){
+    this.carb = this.macroForm.value.carbRange;
+    let carbG = (this.kcal*(this.carb/100))/4;
+    this.carbG = Math.round(carbG/5)*5;
+  }
+
+  energyResult(){
+    if(this.age>=1 && this.age<=2){
+      this.kcal = (this.gender=='M') ? 1000 : 920;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 25; this.maxFat = 35;
+      this.minCarb = 50; this.maxCarb = 69;
+      this.protein = 15; this.fat = 30; this.carb = 55;
+    }else if(this.age>=3 && this.age<=5){
+      this.kcal = (this.gender=='M') ? 1350 : 1260;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 79;
+      this.protein = 15; this.fat = 20; this.carb = 65;
+    }else if(this.age>=6 && this.age<=9){
+      this.kcal = (this.gender=='M') ? 1600 : 1470;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 79;
+      this.protein = 15; this.fat = 20; this.carb = 65;
+    }else if(this.age>=10 && this.age<=12){
+      this.kcal = (this.gender=='M') ? 2060 : 1980;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 79;
+      this.protein = 15; this.fat = 20; this.carb = 65;
+    }else if(this.age>=13 && this.age<=15){
+      this.kcal = (this.gender=='M') ? 2700 : 2170;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 79;
+      this.protein = 15; this.fat = 20; this.carb = 65;
+    }else if(this.age>=16 && this.age<=18){
+      this.kcal = (this.gender=='M') ? 3010 : 2280;
+      this.minProtein = 6; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 79;
+      this.protein = 15; this.fat = 20; this.carb = 65;
     }else{
-      this.minProtein = 6;
-      this.maxProtein = 15;
-
-      this.minFat = 15;
-      this.maxFat = 30;
- 
-      this.minCarbs = 55;
-      this.maxCarbs = 79;
+      this.minProtein = 10; this.maxProtein = 15;
+      this.minFat = 15; this.maxFat = 30;
+      this.minCarb = 55; this.maxCarb = 75;
+      this.protein = 15; this.fat = 20; this.carb = 65;
+      let activity = this.activity;
+      let p = Math.round(((this.height-100)*.1)*100)/100;
+      let dbw = parseInt((Math.round(((this.height-100)-(p))*100)/100).toFixed(2));
+      let kcal = dbw*activity;
+      if(kcal % 50 < 25){
+        kcal -= (kcal % 50);
+      }
+      else if(kcal % 50 > 25){
+        kcal += (50 - (kcal % 50));
+      }
+      else if(kcal % 50 == 25){
+        kcal += 25;
+      }
+      this.kcal = kcal;
     }
-  }
-
-  resetClicked(){
-    this.energySee = true;
-    this.carbsPercentage = 0.65;
-    this.proteinPercentage = 0.15;
-    this.fatPercentage = 0.20;
-    this.modifyDistribution = false;
-    this.calculateDistribution();
-  }
-
-  okClicked(){
-    this.energySee = true; 
-    if(this.totalPercentage == 100){
-      this.modifyDistribution = false;
-      let toast = this.toastCtrl.create({
-        message: 'Changes saved',
-        duration: 3000,
-        position: 'top',
-        showCloseButton: true
-      });
-      toast.present();
-    }
-    else{
-      let toast = this.toastCtrl.create({
-        message: 'Percentage does not equal to 100!',
-        duration: 3000,
-        position: 'top',
-        showCloseButton: true
-      });
-      toast.present();
-    }
-  }
-
-  calculateDistribution(){
-    var carbs:number, protein:number, fat:number;
-
-    if(this.modifyDistribution){
-      carbs = this.carbsPercentage / 100;
-      protein = this.proteinPercentage / 100;
-      fat = this.fatPercentage / 100;
-    }
-    else{
-      carbs = this.carbsPercentage;
-      this.carbsPercentage *= 100;
-
-      protein = this.proteinPercentage;
-      this.proteinPercentage *= 100;
-
-      fat = this.fatPercentage;
-      this.fatPercentage *= 100;
-    }
-      let carbsGrams = (this.energy * carbs) / 4;
-      this.carbsGrams = Math.round( carbsGrams / 5 ) * 5;
-
-      let proteinGrams = (this.energy * protein) / 4;
-      this.proteinGrams = Math.round( proteinGrams / 5 ) * 5;
-
-      let fatGrams = (this.energy * fat) / 9;
-      this.fatGrams = Math.round( fatGrams / 5 ) * 5;
-
-      this.totalPercentage = this.carbsPercentage + this.proteinPercentage + this.fatPercentage;
+    // this.proteinG = Math.round(((this.kcal*(this.protein/100))/4)*100)/100;
+    // this.fatG = Math.round(((this.kcal*(this.fat/100))/9)*100)/100;
+    // this.carbG = Math.round(((this.kcal*(this.carb/100))/4)*100)/100;
+    this.macroForm.controls['proteinRange'].setValue(this.protein);
+    this.macroForm.controls['fatRange'].setValue(this.fat);
+    this.macroForm.controls['carbRange'].setValue(this.carb);
+    let proteinG = (this.kcal*(this.protein/100))/4;
+    let fatG = (this.kcal*(this.fat/100))/9;
+    let carbG = (this.kcal*(this.carb/100))/4;
+    this.proteinG = Math.round(proteinG/5)*5;
+    this.fatG = Math.round(fatG/5)*5;
+    this.carbG = Math.round(carbG/5)*5;
   }
 
   bmiResult(){
-      //getAge && getMonth
-      let weight = this.weight;
-      let height = Math.round((this.height/100)*100)/100;
-      let age = this.getAge();
-      let month = this.getMonth();
-      let gender = this.gender;
-
-      console.log("bmi-weight: " + weight);
-      console.log("bmi-height: " + height);
-      console.log("bmi-age: " + age);
-      console.log("bmi-month: " + month);
-      console.log("bmi-gender: " + gender);
-      //start BMI
-      let bmi = Math.round((weight/(height*height))*100)/100;
-      this.bmi = bmi;
-      //this.bmiMessage = "Your BMI is " + bmi + "kg/m²";
-      if(age<=18 && month>0){
-        this.storage.get('bmi'+gender+age.toString()+month.toString()).then((val)=>{
-          if(bmi<val[0]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "SEVERE THINNESS";
-          }else if(bmi>=val[0] && bmi<val[1]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "UNDERWEIGHT";
-          }else if(bmi>=val[1] && bmi<val[5]){
-            this.bmiAbnormal = false; this.bmiNormal = true; this.bmiStatus = "NORMAL";
-          }else if(bmi>=val[5] && bmi<val[6]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OVERWEIGHT";
-          }else if(bmi>=val[6]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OBESE";
-          }
-        });
-      }else if(age<=19 && month==0){
-        this.storage.get('bmi'+gender+age.toString()).then((val)=>{
-          if(bmi<val[0]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "SEVERE THINNESS";
-          }else if(bmi>=val[0] && bmi<val[1]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "UNDERWEIGHT";
-          }else if(bmi>=val[1] && bmi<val[5]){
-            this.bmiAbnormal = false; this.bmiNormal = true; this.bmiStatus = "NORMAL";
-          }else if(bmi>=val[5] && bmi<val[6]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OVERWEIGHT";
-          }else if(bmi>=val[6]){
-            this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OBESE";
-          }
-        });
-      }else{
-        if(bmi<18.5){
+    //getAge && getMonth
+    let weight = this.weight;
+    let height = Math.round((this.height/100)*100)/100;
+    let age = this.getAge();
+    let month = this.getMonth();
+    let gender = this.gender;
+    //start BMI
+    let bmi = Math.round((weight/(height*height))*100)/100;
+    this.bmi = bmi;
+    //this.bmiMessage = "Your BMI is " + bmi + "kg/m²";
+    if(age<=18 && month>0){
+      this.storage.get('bmi'+gender+age.toString()+month.toString()).then((val)=>{
+        if(bmi<val[0]){
+          this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "SEVERE THINNESS";
+        }else if(bmi>=val[0] && bmi<val[1]){
           this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "UNDERWEIGHT";
-        }else if(bmi>=18.5 && bmi<25){
+        }else if(bmi>=val[1] && bmi<val[5]){
           this.bmiAbnormal = false; this.bmiNormal = true; this.bmiStatus = "NORMAL";
-        }else if(bmi>=25 && bmi<30){
+        }else if(bmi>=val[5] && bmi<val[6]){
           this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OVERWEIGHT";
-        }else if(bmi>=30){
+        }else if(bmi>=val[6]){
           this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OBESE";
         }
+      });
+    }else if(age<=19 && month==0){
+      this.storage.get('bmi'+gender+age.toString()).then((val)=>{
+        if(bmi<val[0]){
+          this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "SEVERE THINNESS";
+        }else if(bmi>=val[0] && bmi<val[1]){
+          this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "UNDERWEIGHT";
+        }else if(bmi>=val[1] && bmi<val[5]){
+          this.bmiAbnormal = false; this.bmiNormal = true; this.bmiStatus = "NORMAL";
+        }else if(bmi>=val[5] && bmi<val[6]){
+          this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OVERWEIGHT";
+        }else if(bmi>=val[6]){
+          this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OBESE";
+        }
+      });
+    }else{
+      if(bmi<18.5){
+        this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "UNDERWEIGHT";
+      }else if(bmi>=18.5 && bmi<25){
+        this.bmiAbnormal = false; this.bmiNormal = true; this.bmiStatus = "NORMAL";
+      }else if(bmi>=25 && bmi<30){
+        this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OVERWEIGHT";
+      }else if(bmi>=30){
+        this.bmiAbnormal = true; this.bmiNormal = false; this.bmiStatus = "OBESE";
       }
+    }
   }
 
   dbwResult(){
@@ -381,48 +390,7 @@ export class ResultsPage {
     }
   }
 
-  energyResult(){
-    this.carbsPercentage = .65;
-    this.proteinPercentage = .15;
-    this.fatPercentage = .20;
-    let height = this.height;
-    let age = this.getAge();
-    this.age = this.getAge();
-    let p = Math.round(((height-100)*.1)*100)/100;
-    console.log(p);
-    let dbw = Math.round(((height-100)-(p))*100)/100;
-    if(age<=2){
-      this.energy = (this.gender=="M") ? 1000 : 920;
-    }else if(age>2 && age<=5){
-      this.energy = (this.gender=="M") ? 1350: 1260;
-    }else if(age>5 && age<=9){
-      this.energy = (this.gender=="M") ? 1600: 147;
-    }else if(age>9 && age<=12){
-      this.energy = (this.gender=="M") ? 2060 : 1980;
-    }else if(age>12 && age<=15){
-      this.energy = (this.gender=="M") ? 2700 : 2170;
-    }else if(age>15 && age<=18){
-      this.energy = (this.gender=="M") ? 3010 : 2280;
-    }else{
-      let round = dbw*this.activity;
-      console.log(round);
-      if(round % 50 < 25){
-        round -= (round % 50);
-      }
-      else if(round % 50 > 25){
-        round += (50 - (round % 50));
-      }
-      else if(round % 50 == 25){
-        round += 25;
-      }
-      this.energy = round;
-    }
-    this.calculateDistribution();
-  }
-
-  activityChange(){
-
-  }
+  
 
   getAge(){
     let today = new Date();
@@ -480,10 +448,7 @@ export class ResultsPage {
   {
     let waist = this.waist;
     let hip = this.hip;
-
-
     this.whRatio = Math.round((waist/hip)*100)/100;
-
     if((this.whRatio >= 0.90) && (this.gender == 'M')){
       this.whOnRisk = true;
     }else if((this.whRatio >= 0.85) && (this.gender == 'F')){
@@ -497,10 +462,7 @@ export class ResultsPage {
   {
     let waist = this.waist;
     let height = this.height;
-
-
     this.whgtRatio = Math.round((waist/height)*100)/100;
-
     if(this.whgtRatio <= 0.50){
        this.whgtOnRisk = false;
      }else{
